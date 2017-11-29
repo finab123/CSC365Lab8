eb18bf206708f18d0586129ebc552a9e55cd5b69
Fina Beauchamp <finab123@Finas-MacBook-Pro.local>
2017-11-29 09:27:53 -0800

    Adding initial files

---
+++ InnReservations.java
@@ -0,0 +1,9 @@
+/* CSC 365 -- Lab 8
+ * Fina Beauchamp (smbeauch) and Brandon Newby
+ */
+public class InnReservations {
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+
+}
