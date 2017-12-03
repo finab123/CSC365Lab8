7d54a7b64c50e8b3a02caf6118ee18a895b5a365
brandon <brandon@Brandons-MacBook-Pro.local>
2017-12-02 21:20:12 -0800

    Finished AdminMenu

--- AdminMenu.java
+++ AdminMenu.java
@@ -2,14 +2,20 @@
  * Fina Beauchamp (smbeauch) and Brandon Newby
  */
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.*;
+import java.util.Date;
 
 import javax.print.DocFlavor.STRING;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class AdminMenu {
 
+    private static final String ROOM_TABLE = "testRoom";
+    private static final String RES_TABLE = "testRes"; 
     private Connection conn = null;
     private Scanner inputScanner = null;
 
@@ -20,11 +26,33 @@ public class AdminMenu {
 	}
 
 	public static void getDBStatus(Connection conn) {
+        
+        int resCount = getReservationCount(conn);
+        int roomCount = getRoomCount(conn);
+        
+        System.out.print("Reservations Status: ");
+        if (resCount >= 600) {
 
-        System.out.println("Getting DB Status...");
-        System.out.println("Database Status: functionNotWritten");
-        System.out.println("Reservations: " + getReservationCount(conn));
-        System.out.println("Rooms: " + getRoomCount(conn));
+            System.out.print("Full");
+        } else if (resCount == 0) {
+            System.out.print("Empty");
+        } else {
+            System.out.print("No Database");
+        }
+        System.out.println(" - Count: " + (resCount == -1 ? 0: resCount));
+
+
+        System.out.print("Rooms Status: ");
+        if (roomCount >= 10) {
+
+            System.out.print("Full");
+        } else if (roomCount == 0) {
+            System.out.print("Empty");
+        } else {
+            System.out.print("No Database");
+        }
+        System.out.println(" - Count: " + (roomCount == -1 ? 0: roomCount));
+
 	}
 
     private void switchSubSystem() {
@@ -35,7 +63,7 @@ public class AdminMenu {
 	private static void printMenuOptions() {
 
         
-        System.out.println("Select an action by number: ");
+        System.out.println("\nSelect an action by number: ");
         System.out.println("[0] Show Menu Options");  
         System.out.println("[1] Database Status");
         System.out.println("[2] Display Table");
@@ -52,6 +80,7 @@ public class AdminMenu {
 		String option;
         boolean isValid = true;
         ClearScreen.clear();
+        getDBStatus(conn);
     	printMenuOptions();
 
     	while(isValid) {
@@ -61,7 +90,6 @@ public class AdminMenu {
                 printMenuOptions();
                 break;
             case "1":
-                getDBStatus(conn);
                 break;
             case "2":
                 displayTable();
@@ -84,20 +112,103 @@ public class AdminMenu {
                isValid = true;
                break;
           }
-          System.out.println("Please Enter Another Command");
+          getDBStatus(conn);
+          System.out.println("Please Enter Another Command:");
         }
         return 0;
     }
 
-    private void displayTable() {
-        System.out.println("Displaying Table...");
-        
+    private void displayRooms() {
+
+        try {
+            
+            String count = "SELECT * FROM " + ROOM_TABLE;
+            
+            Statement ps = conn.createStatement();
+            ResultSet rs = ps.executeQuery(count);
+            
+            while (rs.next()) {
+                
+                System.out.print(rs.getString(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getInt(3) + ", ");
+                System.out.print(rs.getString(4) + ", ");
+                System.out.print(rs.getInt(5) + ", ");
+                System.out.print(rs.getInt(6) + ", ");
+                System.out.println(rs.getString(7));
+
+            }
+
+        } catch (Exception e) {
+
+        }
 
 
     }
 
+    private void displayReservations() {
+
+        try {
+
+            String count = "SELECT * FROM " + RES_TABLE;
+            
+            Statement ps = conn.createStatement();
+            ResultSet rs = ps.executeQuery(count);
+            
+            while (rs.next()) {
+                
+                System.out.print(rs.getInt(1) + ", ");
+                System.out.print(rs.getString(2) + ", ");
+                System.out.print(rs.getDate(3) + ", ");
+                System.out.print(rs.getDate(4) + ", ");
+                System.out.print(rs.getBigDecimal(5) + ", ");
+                System.out.print(rs.getString(6) + ", ");
+                System.out.print(rs.getString(7) + ", ");
+                System.out.print(rs.getInt(8) + ", ");
+                System.out.println(rs.getInt(9));
+
+            }
+
+        } catch (Exception e) {
+        }
+    }
+    private void displayTable() {
+
+        System.out.println("\nSelect a table to display or anything else to exit:");
+        System.out.println("[0] Display Rooms");
+        System.out.println("[1] Display Reservations");
+
+        switch(inputScanner.next()) {
+            case "0":
+                displayRooms();
+                break;
+            case "1":
+                displayReservations();
+                break;
+            default:
+
+        }
+
+    }
+
     private void clearDB() {
-        System.out.println("Clearing DB...");
+
+        try {
+            
+            String setup = "DELETE FROM " + ROOM_TABLE;
+            
+            Statement s1 = conn.createStatement();
+            s1.executeUpdate(setup);
+
+            setup = "DELETE FROM " + RES_TABLE;
+            s1.execute(setup);
+            
+        }
+        catch (Exception e) {
+ 
+            System.out.println("ee96: " + e);
+        }
+
 
 
     }
@@ -105,7 +216,7 @@ public class AdminMenu {
     private void createTableRooms() {
         try {
             
-            String setup = "CREATE TABLE rooms("
+            String setup = "CREATE TABLE " + ROOM_TABLE + "("
 
                          + "id CHAR(3) PRIMARY KEY,"
                          + "name CHAR(40),"
@@ -121,7 +232,6 @@ public class AdminMenu {
         }
         catch (Exception e) {
  
-            System.out.println("ee96: " + e);
         }
     }
 
@@ -129,10 +239,10 @@ public class AdminMenu {
 
         try {
             
-            String setup = "CREATE TABLE reservations("
+            String setup = "CREATE TABLE " + RES_TABLE + "("
 
                          + "code INT PRIMARY KEY,"
-                         + "room CHAR(3) REFERENCES rooms(id),"
+                         + "room CHAR(3) REFERENCES " + ROOM_TABLE + "(id),"
                          + "check_in DATE,"
                          + "check_out DATE,"
                          + "rate DECIMAL(5,2),"
@@ -145,12 +255,15 @@ public class AdminMenu {
             
             Statement s1 = conn.createStatement();
             s1.executeUpdate(setup);
-            
         }
         catch (Exception e) {
  
-            System.out.println("ee96: " + e);
         }
+    }
+
+    // removes quotes from strings
+    private String trim(String in) {
+        return in.substring(1, in.length() - 1); 
     }
 
     private void populateRooms() {
@@ -160,7 +273,7 @@ public class AdminMenu {
             //remove the first line which holds the column names;
             String line = roomScanner.nextLine();
 
-            String psText = "INSERT INTO rooms VALUES (?,?,?,?,?,?,?)";
+            String psText = "INSERT INTO " + ROOM_TABLE + " VALUES (?,?,?,?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(psText);
 
             while (roomScanner.hasNextLine()) {
@@ -168,81 +281,87 @@ public class AdminMenu {
                 line = roomScanner.nextLine();
                 String[] args = line.split(",");
 
-                ps.setString(1, args[0]);
-                ps.setString(2, args[1]);
+                ps.setString(1, trim(args[0]));
+                ps.setString(2, trim(args[1]));
                 ps.setInt(3, Integer.parseInt(args[2]));
-                ps.setString(4, args[3]);
+                ps.setString(4, trim(args[3]));
                 ps.setInt(5, Integer.parseInt(args[4]));
                 ps.setInt(6, Integer.parseInt(args[5]));
-                ps.setString(7, args[6]);
+                ps.setString(7, trim(args[6]));
 
                 ps.executeUpdate();
 
             }
        }
        catch(Exception e) {
-          System.out.println("Exception " + e);
-          System.exit(-1);
+          //System.out.println("Exception " + e);
+          
        }
         
     }
 
     private void populateReservations() {
        
-    //     try {
-    //         Scanner resScanner = new Scanner(new File("SQLFiles/Reservations.csv"));
-    //         //remove the first line which holds the column names;
-    //         String line = resScanner.nextLine();
+        try {
+            Scanner resScanner = new Scanner(new File("SQLFiles/Reservations.csv"));
+            //remove the first line which holds the column names;
+            String line = resScanner.nextLine();
 
-    //         String psText = "INSERT INTO reservations VALUES (?,?,?,?,?,?,?,?,?)";
-    //         PreparedStatement ps = conn.prepareStatement(psText);
+            String psText = "INSERT INTO " + RES_TABLE + " VALUES (?,?,?,?,?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(psText);
 
-    //         while (resScanner.hasNextLine()) {
+            while (resScanner.hasNextLine()) {
 
-    //             line = resScanner.nextLine();
-    //             String[] args = line.split(",");
+                line = resScanner.nextLine();
+                String[] args = line.split(",");
 
-    //             ps.setInt(1, Integer.parseInt(args[0]));
-    //             ps.setString(2, args[1]);
-    //             ps.setString(3, args[2]);
-    //             ps.setString(4, args[3]);
-    //             ps.setInt(5, Integer.parseInt(args[4]));
-    //             ps.setInt(6, Integer.parseInt(args[5]));
-    //             ps.setString(7, args[6]);
+                DateFormat format = new SimpleDateFormat("d-MMM-yy", Locale.ROOT);
 
-    //             ps.executeUpdate();
+                ps.setInt(1, Integer.parseInt(args[0]));
+                ps.setString(2, trim(args[1]));
+                ps.setDate(3, new java.sql.Date(format.parse(trim(args[2])).getTime()));
+                ps.setDate(4, new java.sql.Date(format.parse(trim(args[3])).getTime()));
+                ps.setBigDecimal(5, new BigDecimal(args[4]));
+                ps.setString(6, trim(args[5]));
+                ps.setString(7, trim(args[6]));
+                ps.setInt(8, Integer.parseInt(args[7]));
+                ps.setInt(9, Integer.parseInt(args[8]));
 
-    //         }
-    //    }
-    //    catch(Exception e) {
-    //       System.out.println("Exception " + e);
-    //       System.exit(-1);
-    //    }
+                ps.executeUpdate();
+
+            }
+       }
+       catch(Exception e) {
+          //System.out.println("Exception " + e);
+         
+       }
         
     }
 
     private void reloadDB() {
-        System.out.println("Reloading DB...");
+        
+        clearDB();
         createTableRooms();
         createTableReservations();
         populateRooms();
         populateReservations();
+        System.out.println("DataBase Reloaded");
     }
     
 
     private void removeDB() {
 
-        System.out.println("Removing DB...");
         try {
             
-            String drop = "DROP TABLE reservations, rooms";
+            String drop = "DROP TABLE " + RES_TABLE + ", " + ROOM_TABLE;
             
-            Statement ps = conn.createStatement();
-            ResultSet rs = ps.executeQuery(drop);
+            Statement s1 = conn.createStatement();
+            s1.executeUpdate(drop);
 
         } catch (Exception e) {
             System.out.println("Could not drop tables" + e);
         }
+        System.out.println("DataBase dropped");
 
     }
 
@@ -252,7 +371,7 @@ public class AdminMenu {
 
         try {
 
-            String count = "SELECT COUNT(*) FROM reservations";
+            String count = "SELECT COUNT(*) FROM " + RES_TABLE;
             
             Statement ps = conn.createStatement();
             ResultSet rs = ps.executeQuery(count);
@@ -263,7 +382,7 @@ public class AdminMenu {
             }
 
         } catch (Exception e) {
-            System.out.println("Could not getReservationCount" + e);
+            numRes = -1;
         }
         return numRes;
     }
@@ -273,7 +392,7 @@ public class AdminMenu {
         int numRooms = 0;
         try {
 
-            String count = "SELECT COUNT(*) FROM rooms";
+            String count = "SELECT COUNT(*) FROM " + ROOM_TABLE;
             
             Statement s1 = conn.createStatement();
             ResultSet rs = s1.executeQuery(count);
@@ -284,7 +403,7 @@ public class AdminMenu {
             }
 
         } catch (Exception e) {
-            System.out.println("Could not getRoomCount" + e);
+            numRooms = -1;
         }
         return numRooms;
     }
